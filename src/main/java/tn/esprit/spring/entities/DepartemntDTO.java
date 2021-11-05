649e8276c205d3043fe96377819b79ff17084c81
wahib <abdennebi.wahib1@esprit.tn>
2021-11-05 17:57:16 +0100

    gestion de entreprise

---
+++ src/main/java/tn/esprit/spring/entities/DepartemntDTO.java
@@ -0,0 +1,19 @@
+package tn.esprit.spring.entities;
+
+public class DepartemntDTO {
+	
+	private String name;
+
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ src/test/java/tn/esprit/spring/EntrepriseTest.java
@@ -0,0 +1,49 @@
+package tn.esprit.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.services.IEntrepriseService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseTest {
+	@Autowired
+	IEntrepriseService ie;
+
+	@Test
+	public void ajouterEntrepriseTest() {
+		Entreprise e = new Entreprise("test", "testtest");
+		Assert.assertNotEquals(0, ie.ajouterEntreprise(e));
+		ie.ajouterEntreprise(e);
+	}
+
+	@Test
+	public void ajouterDepartementTest() {
+		Departement d = new Departement("test");
+		Assert.assertNotEquals(0, ie.ajouterDepartement(d));
+		ie.ajouterDepartement(d);
+	}
+
+	@Test
+	public void affecterDepartementAEntrepriseTest() {
+		
+		ie.affecterDepartementAEntreprise(1,5);
+	}
+
+	@Test
+	public void getAllDepartementsNamesByEntreprise() {
+		Assert.assertNotEquals(0, ie.getAllDepartementsNamesByEntreprise(5).size());
+	}
+
+	
+
+	
+
+}
