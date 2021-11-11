7b0e050e1a87cc6fa1b3a3773c4e0ddc8316942f
houssemkhemiri <57456999+houssemkhemiri@users.noreply.github.com>
2021-11-11 02:18:28 +0100

    test unit

--- src/test/java/tn/esprit/spring/MissionTest.java
+++ src/test/java/tn/esprit/spring/MissionTest.java
@@ -1,13 +1,31 @@
 package tn.esprit.spring;
 
-import java.util.logging.LogManager;
+import static org.junit.Assert.assertThat;
 
+import java.text.ParseException;
+import java.util.logging.LogManager;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.services.ITimesheetService;
 
 public class MissionTest {
 	@Autowired
-	DepartementRepository departementRepository;
+	ITimesheetService  im;
+	
+	@Test
+	public  void ajouterMissionTest() throws ParseException {
+		Mission m = new Mission();
+		m.setId(10);
+		m.setName("SagemCom");
+		m.setDescription("embarqué");
+		im.ajouterMission(m);
+		assertThat(m.getName()).isEqualTo("sagem");
+
+	}
 
 }
